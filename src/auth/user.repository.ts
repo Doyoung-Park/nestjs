79833482afed45ea6d9ec78a908b9a5dcd2332c0
Doyoung <doodoo55@gachon.ac.kr>
2023-01-25 23:33:39 +0900

    feat: password 암호화 저장

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,7 +1,8 @@
-import { Injectable } from "@nestjs/common";
+import { ConflictException, Injectable, InternalServerErrorException } from "@nestjs/common";
 import { DataSource, Repository } from "typeorm";
 import { AuthCredentialDto } from "./dto/auth-credential.dto";
 import { User } from "./user.entity";
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UserRepository extends Repository<User>{
@@ -11,12 +12,23 @@ export class UserRepository extends Repository<User>{
 
     async createUser(AuthCredentialDto: AuthCredentialDto):Promise<void>{
         const {username, password} = AuthCredentialDto;
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(password, salt);
 
         const user = await this.create({
             username,
-            password,
+            password: hashedPassword,
         });
-
-        await this.save(user);
+        try {
+            await this.save(user);    
+        } catch (error) {
+            console.error(error);
+            if(error.code ==='23505'){
+                throw new ConflictException('Existing username');
+            }else{
+                throw new InternalServerErrorException();
+            }
+        }
+        
     }
 }
